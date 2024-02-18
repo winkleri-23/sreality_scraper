29286c1c3408ad5bac00a7991da7efbd22fe99cf
Erich Winkler <erich.winkler23@gmail.com>
2024-02-18 20:40:15 +0100

    comments added

--- srealityScraper/srealityScraper/spiders/flats.py
+++ srealityScraper/srealityScraper/spiders/flats.py
@@ -1,24 +1,30 @@
 import scrapy
-import json
 from srealityScraper.items import FlatItem
 
 class FlatSpider(scrapy.Spider):
     name = 'sreality'
-
     start_urls = ['https://www.sreality.cz/api/cs/v2/estates?category_main_cb=1&category_type_cb=1&sort=0&per_page=500']
 
     def parse(self, response):
-         jsonresponse = response.json()
-         for item in jsonresponse["_embedded"]['estates']:
-             yield scrapy.Request( 'https://www.sreality.cz/api' + item['_links']['self']['href'] ,
-                          callback=self.parse_flat)
+        # Parse the JSON response
+        jsonresponse = response.json()
+
+        # Iterate over each item in the response
+        for item in jsonresponse["_embedded"]['estates']:
+            # Send a request to the URL of each item
+            yield scrapy.Request('https://www.sreality.cz/api' + item['_links']['self']['href'], callback=self.parse_flat)
 
     def parse_flat(self, response):
+        # Parse the JSON response
         jsonresponse = response.json()
-        flat = FlatItem()
-        flat['title'] = jsonresponse['name']['value']
-        #item["ADDRESS"] = jsonresponse['locality']['value']
 
+        # Create a new FlatItem object
+        flat = FlatItem()
+
+        # Extract the title from the response and assign it to the FlatItem object
+        flat['title'] = jsonresponse['name']['value']
+
+        # Extract the image URL from the response and assign it to the FlatItem object
         for images in jsonresponse['_embedded']['images']:
             if images['_links']['dynamicDown']:
                 tmp = images['_links']['dynamicDown']['href'].replace('{width}', '400')
@@ -26,4 +32,5 @@ class FlatSpider(scrapy.Spider):
                 flat['image_urls'] = tmp
                 break
 
+        # Yield the FlatItem object
         yield flat
