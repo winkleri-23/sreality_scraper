68c02766b731d56c5881d5eb584391dd8a6d007b
Erich Winkler <erich.winkler23@gmail.com>
2024-02-18 11:09:45 +0100

    http server in docker

--- http_server/server.py
+++ http_server/server.py
@@ -1,12 +1,15 @@
 import http.server
 import socketserver
+from http import HTTPStatus
+
+        
 
 class HttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(b'Hello, World!')
+        self.wfile.write(b'Hello world')
+
 
 with socketserver.TCPServer(('', 8080), HttpRequestHandler) as server:
     print('Server started on port 8080...')
