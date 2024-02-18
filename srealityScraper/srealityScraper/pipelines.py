be771196d3b11adac4a8fd098b487314bcdbe7eb
Erich Winkler <erich.winkler23@gmail.com>
2024-02-18 20:18:33 +0100

    Scraper - database connection

--- srealityScraper/srealityScraper/pipelines.py
+++ srealityScraper/srealityScraper/pipelines.py
@@ -6,8 +6,63 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import psycopg2
+import time
 
 
 class SrealityscraperPipeline:
+    def __init__(self):
+        hostname = '127.0.0.1'
+        port = '5432'
+        username = 'test'
+        password = 'test'
+        database = 'sreality'
+
+        # Connect to the database
+        attempts = 0
+        connected = False
+        max_attempts = 3
+        while not connected and attempts < max_attempts:
+            try:
+                self.connection = psycopg2.connect(host='database', user=username, password=password, dbname=database, port=port)
+                connected = True
+            except psycopg2.OperationalError:
+                attempts += 1
+                print("Failed to connect to the database. Retrying in 5 seconds...")
+                time.sleep(3)
+        if not connected:
+            print("Failed to connect to the database after maximum attempts. Exiting...")
+            return
+
+        # handle for exectuing SQL commands
+        self.cur = self.connection.cursor()
+
+        # create flat table if not exists
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS flat(
+            id serial PRIMARY KEY,
+            title text,
+            image_urls text
+        )
+        """)
+        
+        # delete all records from flat table
+        self.cur.execute("""
+        DELETE FROM flat *
+        """)
+
     def process_item(self, item, spider):
+        ## Define insert statement
+        self.cur.execute(""" insert into flat (title, image_urls) values (%s,%s)""", (
+            item["title"],
+            str(item["image_urls"])
+        ))
+        ## Execute insert of data into database
+        self.connection.commit()
+
+        print('Data inserted into database successfully')
         return item
+
+    def close_spider(self, spider):
+        self.connection.close()
+
--- srealityScraper/srealityScraper/settings.py
+++ srealityScraper/srealityScraper/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "srealityScraper.spiders"
 #USER_AGENT = "srealityScraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "srealityScraper.pipelines.SrealityscraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "srealityScraper.pipelines.SrealityscraperPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -85,7 +85,7 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = "httpcache"
 #HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
